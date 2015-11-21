c75865a0f9d58ddee177f983c7e6829124cc3ca2
Meisolsson <henrikolsson@outlook.com>
2015-11-21 16:57:51 +0100

    Resets game after score

--- game.js
+++ game.js
@@ -3,14 +3,23 @@ var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', { preload: preload, creat
 var PLAYER_SPEED_CHANGE = 15;
 var BALL_SPEED_CHANGE = 16.5;
 
-var platforms;
+//Players
 var playerOne;
 var PlayerTwo;
-var ball;
-var score = 0;
 
+//Static objects
 var playerOneGoal;
 var playerTwoGoal;
+var ball;
+
+//Game data
+var score = 0;
+var text;
+var resetting
+
+//
+var timer;
+var stateManager;
 
 var text;
 
@@ -23,8 +32,13 @@ function preload() {
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE)
 	
-    ball = game.add.sprite(game.world.width/2, game.world.height/2, 'ball');
-    playerOne = game.add.sprite(game.world.width/2 - 100, game.world.height/2 - 16, 'player');
+    stateManager = new Phaser.StateManager(this.game);
+    timer = game.time.create(false);
+    
+    
+    text = game.add.text(200, 200, score, {"fill":"white"});
+    ball = game.add.sprite(128, game.world.height - 150, 'ball');
+    playerOne = game.add.sprite(1, game.world.height - 150, 'player');
     playerOneGoal = game.add.sprite(50, 100, 'ground');
     playerTwoGoal = game.add.sprite(1125, 100, 'ground');
     
@@ -38,27 +52,25 @@ function create() {
     
     playerOne.body.maxVelocity = new Phaser.Point(160, 160);
     playerOne.body.drag = new Phaser.Point(300, 300);
-	
+
+    ball.body.maxVelocity = new Phaser.Point(200, 200);
 	ball.body.bounce = new Phaser.Point(0.4, 0.4);
 	ball.body.drag = new Phaser.Point(35, 35);
     ball.body.collideWorldBounds = true;
     ball.tint = Math.random() * 0xFFFFFF<<0;
     
-    text = game.add.text(200, 200, score, {"fill":"white"});
-    
-}
-
-function setGameOver(){
-    text.setText(score++);
 }
 
 function update() {
+    if(resetting)
+        return;
+    
 	game.physics.arcade.collide(playerOne, ball);
     game.physics.arcade.collide(ball, playerOneGoal, setGameOver);
     game.physics.arcade.collide(ball, playerTwoGoal, setGameOver);
     
 	var cursors = game.input.keyboard.createCursorKeys();
-
+    
     if (cursors.left.isDown){
         playerOne.body.velocity.x -= PLAYER_SPEED_CHANGE;
     } else if (cursors.right.isDown){
@@ -76,7 +88,24 @@ function update() {
         var velX = Math.cos(angle) * BALL_SPEED_CHANGE;
         var velY = Math.sin(angle) * BALL_SPEED_CHANGE;
         
-        console.log(angle + " :: " + velX  + " :: " + velY)
         ball.body.velocity.add(velX, velY);
     }
+}
+    
+function setGameOver(){
+    text.setText(score++);
+    resetting = true;
+    
+    timer.add(3000, resetField, this);
+    timer.start();
+}
+
+function resetField(){
+    var ballCenterX = (game.width / 2) - (ball.width / 2);
+    var ballCenterY = (game.height / 2) - (ball.height / 2);
+    ball.position.set(ballCenterX, ballCenterY);
+    
+    var playerCenterY = (game.height / 2) - (playerOne.height / 2);
+    playerOne.position.set(playerOneGoal.x + 100, playerCenterY);
+    resetting = false;
 }
