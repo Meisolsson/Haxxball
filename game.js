7df8565403b990268d78fe6695d6a488eead85db
emil225522 <ungothar@hotmail.com>
2015-11-21 12:09:39 +0100

    Added goals and made the screen bigger.

--- game.js
+++ game.js
@@ -1,19 +1,25 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var PLAYER_SPEED_CHANGE = 15;
-var BALL_SPEED_CHANGE = 40;
+var BALL_SPEED_CHANGE = 10;
 
 var platforms;
-var player;
+var playerOne;
+var PlayerTwo;
 var ball;
+var score = 0;
+
+var playerOneGoal;
+var playerTwoGoal;
 
 function preload() {
 	game.load.image('ground', 'assets/platform.png');
 	game.load.image('player', 'assets/player.png');
+    game.load.image('ball', 'assets/ball.png');
 }
 
 function create() {
-	game.physics.startSystem(Phaser.Physics.ARCADE)
+	//game.physics.startSystem(Phaser.Physics.ARCADE)
 	
 	/*platforms = game.add.group();
     platforms.enableBody = true;
@@ -22,41 +28,52 @@ function create() {
     ground.body.immovable = true;*/
 	
 	
-	player = game.add.sprite(16, game.world.height - 150, 'player');
-    game.physics.arcade.enable(player);
-    player.body.maxVelocity = new Phaser.Point(90, 90);
-    player.body.drag = new Phaser.Point(20, 20);
-    player.body.collideWorldBounds = true;
+	playerOne = game.add.sprite(1, game.world.height - 150, 'player');
+    playerOneGoal = game.add.sprite(50,100, 'ground');
+    playerTwoGoal = game.add.sprite(1125,100, 'ground');
+    game.physics.arcade.enable(playerOne);
+    game.physics.arcade.enable(playerOneGoal);
+    game.physics.arcade.enable(playerTwoGoal);
+    playerOneGoal.body.immovable = true;
+    playerTwoGoal.body.immovable = true;
+    playerOne.body.maxVelocity = new Phaser.Point(160, 160);
+    playerOne.body.drag = new Phaser.Point(300, 300);
+    //player.body.collideWorldBounds = true;
 	
-	ball = game.add.sprite(128, game.world.height - 150, 'player');
+	ball = game.add.sprite(128, game.world.height - 150, 'ball');
     game.physics.arcade.enable(ball);
 	
     ball.scale = new Phaser.Point(0.7, 0.7);
 	ball.body.bounce = new Phaser.Point(0.4, 0.4);
-	ball.body.drag = new Phaser.Point(30, 30);
+	ball.body.drag = new Phaser.Point(50, 50);
     ball.body.collideWorldBounds = true;
+    
     ball.tint = Math.random() * 0xFFFFFF<<0;
 }
+function setGameOver(){
 
+}
 function update() {
-	game.physics.arcade.collide(player, ball);
-	
+	game.physics.arcade.collide(playerOne, ball);
+    game.physics.arcade.collide(ball,playerOneGoal);
+    game.physics.arcade.collide(ball,playerTwoGoal);
+    this.physics.arcade.collide(this.ball, this.playerOneGoal, this.setGameOver, null, this);
 	var cursors = game.input.keyboard.createCursorKeys();
 
     if (cursors.left.isDown){
-        player.body.velocity.x -= PLAYER_SPEED_CHANGE;
+        playerOne.body.velocity.x -= PLAYER_SPEED_CHANGE;
     } else if (cursors.right.isDown){
-        player.body.velocity.x += PLAYER_SPEED_CHANGE;
+        playerOne.body.velocity.x += PLAYER_SPEED_CHANGE;
 	}
     
     if (cursors.up.isDown){
-        player.body.velocity.y -= PLAYER_SPEED_CHANGE;
+        playerOne.body.velocity.y -= PLAYER_SPEED_CHANGE;
     }else if(cursors.down.isDown){
-		player.body.velocity.y += PLAYER_SPEED_CHANGE;
+		playerOne.body.velocity.y += PLAYER_SPEED_CHANGE;
 	}
     
-    if(game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR) && game.physics.arcade.distanceBetween(player, ball) < 46){
-        var angle = game.physics.arcade.angleBetween(player, ball);
+    if(game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR) && game.physics.arcade.distanceBetween(playerOne, ball) < 46){
+        var angle = game.physics.arcade.angleBetween(playerOne, ball);
         var velX = Math.cos(angle) * BALL_SPEED_CHANGE;
         var velY = Math.sin(angle) * BALL_SPEED_CHANGE;
         ball.body.velocity.add(velX, velY);
